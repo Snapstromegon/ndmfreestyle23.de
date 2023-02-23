9f7700764ed476f8b48f8d4c0f971234d16259d0
Raphael Höser <raphael@hoeser.info>
2023-02-23 20:24:47 +0100

    🚀 Initial Commit

---
+++ .eleventy.js
@@ -0,0 +1,139 @@
+const yaml = require("js-yaml");
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const Image = require("@11ty/eleventy-img");
+const typescript = require("@rollup/plugin-typescript");
+const { default: resolve } = require("@rollup/plugin-node-resolve");
+const markdownItEmoji = require("markdown-it-emoji");
+const markdownItContainer = require("markdown-it-container");
+const rollupPlugin = require("eleventy-plugin-rollup");
+
+function generateImages(src) {
+  return Image(src, {
+    formats: [
+      "avif",
+      "webp",
+      src.toLowerCase().endsWith(".png") ? "png" : "jpeg",
+    ],
+    outputDir: "_site/img/",
+    widths: [256, 512, 1024, null],
+  });
+}
+
+async function imageShortcode(src, alt, sizes = []) {
+  const metadata = await generateImages(src);
+
+  const imageAttributes = {
+    alt,
+    decoding: "async",
+    loading: "lazy",
+    sizes,
+  };
+
+  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
+function generateFavicon(src) {
+  return Image(src, {
+    formats: ["svg", "png", "webp", "avif"],
+    outputDir: "_site/img/",
+    widths: [64, 128, 180, 256, 512, 1024, 2048, null],
+  });
+}
+
+async function generateFaviconHTML(src) {
+  const metadata = await generateFavicon(src);
+  return `
+    <link rel="icon" href="${metadata.png[0].url}" type="image/png">
+    <link rel="apple-touch-icon" href="${
+      metadata.png.find(png => png.width === 180).url
+    }">
+  `;
+}
+
+const registerPlugins = eleventyConfig => {
+  eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(rollupPlugin, {
+    rollupOptions: {
+      output: {
+        dir: "_site/js",
+        format: "es",
+        sourcemap: process.env.NETLIFY !== "true",
+      },
+      plugins: [typescript(), resolve()],
+    },
+  });
+};
+
+const registerFileConfigs = eleventyConfig => {
+  eleventyConfig.addWatchTarget("src/css");
+  eleventyConfig.addWatchTarget("assets/js/");
+  eleventyConfig.addPassthroughCopy("assets/img");
+  eleventyConfig.addPassthroughCopy("assets/video");
+  eleventyConfig.addPassthroughCopy("assets/fonts");
+};
+
+const addFilters = eleventyConfig => {
+  eleventyConfig.addFilter("logging", (input, label, passthrough) => {
+    console.log(`logging-${label}:`, input);
+    if (passthrough) return input;
+  });
+  eleventyConfig.addFilter("encodeURIComponent", encodeURIComponent);
+  eleventyConfig.addNunjucksAsyncFilter("faviconData", (src, callback) => generateFavicon(src).then(data => callback(null, data)));
+  eleventyConfig.addNunjucksAsyncFilter("imageData", (src, callback) => generateImages(src).then(data => callback(null, data)));
+  eleventyConfig.addFilter("niceDate", date => {
+    const months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Okt",
+      "Nov",
+      "Dec",
+    ];
+    return `${date.getDate()}. ${
+      months[date.getMonth()]
+    } ${date.getFullYear()}`;
+  });
+};
+
+const addShortcodes = eleventyConfig => {
+  eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
+  eleventyConfig.addDataExtension("yml", contents => yaml.load(contents));
+  eleventyConfig.addShortcode("currentTime", () => Date.now().toString());
+  eleventyConfig.addAsyncShortcode("image", imageShortcode);
+  eleventyConfig.addAsyncShortcode("favicon", generateFaviconHTML);
+};
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.amendLibrary("md", mdLib => mdLib
+    .set({
+      breaks: true,
+      html: true,
+      linkify: true,
+    })
+    .use(markdownItEmoji)
+    .use(markdownItContainer, "sidenote")
+    .use(markdownItContainer, "commentBlock")
+    .use(markdownItContainer, "reader-thought")
+    .use(markdownItContainer, "writer-thought"));
+  registerPlugins(eleventyConfig);
+
+  addShortcodes(eleventyConfig);
+  addFilters(eleventyConfig);
+
+  registerFileConfigs(eleventyConfig);
+
+  // Return your Object options:
+  return {
+    dir: { input: "src" },
+    markdownTemplateEngine: "njk",
+  };
+};
---
+++ src/index.md
@@ -0,0 +1,30 @@
+---
+layout: layouts/main.njk
+eleventyNavigation:
+  key: Start
+  order: 1
+---
+
+<header>
+  <div class="content">
+    <span class="underline_fancy">Kommt bald...</span>
+    <h1>Hier kommen bald mehr Informationen</h1>
+  </div>
+</header>
+
+<main>
+  <div class="content">
+    <article>
+
+# Hallo!
+
+Wir freuen uns über dein Interesse an unserer Veranstaltung und wollen dir demnächst auf unserer Homepage alle Informationen zur Verfügung stellen, die für den Besuch oder die Teilnahme an unserer Meisterschaft 2019 nützlich sind.
+
+Falls trotzdem Fragen offen bleiben kannst du dich jederzeit an folgende E Mail Adresse wenden: [info@ndmfreestyle23.de](mailto:info@ndmfreestyle23.de).
+
+Bis bald in unserem schönen Luftkurort!
+Dein Orga Team vom SSV Nümbrecht
+
+  </article>
+  </div>
+</main>
