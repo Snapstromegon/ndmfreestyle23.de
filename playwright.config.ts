ed5a415b17cfdaed1403b24435782b5f62a3e52f
Raphael Höser <raphael@hoeser.info>
2023-03-08 23:23:02 +0100

    Fix playwright config linting
    Signed-off-by: Raphael Höser <raphael@hoeser.info>

--- playwright.config.ts
+++ playwright.config.ts
@@ -83,8 +83,8 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run dev",
-    url: "http://localhost:8888",
     reuseExistingServer: true,
+    url: "http://localhost:8888",
   },
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : "50%",
